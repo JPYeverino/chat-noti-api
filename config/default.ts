1eb3939570ce4d47bedad82e0a25781e13b752d7
JPYeverino <jp.yeverinom@gmail.com>
2019-04-23 16:13:13 -0700

    AWS Ready

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     HOST: 'http://noti-api',
     PORT: 3001,
-    MONGO_URI: 'mongodb://mongo:27017/noti-api',
+    MONGO_URI: 'mongodb://mongonoti:27017/noti-api',
     USER_API: 'http://auth-api:3000/users'
 }
